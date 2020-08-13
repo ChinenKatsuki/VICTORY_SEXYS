c3228e5ac5043d1b1964f17c034ab2d0093f4558
ChinenKatsuki <67732612+ChinenKatsuki@users.noreply.github.com>
2020-08-13 19:09:35 +1000

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -18,8 +18,8 @@
 
 require_once('./LINEBotTiny.php');
 
-$channelAccessToken = '1VzWykZ2v1j13XZzj+FUZ3M5i+rUM2vpvmevUvz2iHucHxlM7G4Ep/BHIAu5KF6WQ+dnFZE0WvKbnZSVuBbEU1kiimisa17ldsBDgl8Qsv57eXtxf6Iyv96zCn573msBCu93Ja2TpozqU4DLypfQrAdB04t89/1O/w1cDnyilFU=';
-$channelSecret = '3c29753317491acf27e2d2879fe9e43a';
+$channelAccessToken = 'チャンネルアクセストークンID';
+$channelSecret = 'チャンネルセレクトID';
 
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
 foreach ($client->parseEvents() as $event) {
